8be8458f9b9a98e7e56a42392030af6dfb5b0f51
Daniel Marrama <danielmarrama@gmail.com>
2023-03-19 22:49:58 -0700

    random set paths

--- random_sets.py
+++ random_sets.py
@@ -18,18 +18,23 @@ def make_random_sets():
   human_proteome = list(SeqIO.parse('data/non_myocarditis_antigens.fasta', 'fasta'))
   
   for i in range(1000):
-    with open(f'random_sets/random_protein_set_{i+1}.fasta', 'w') as f:
+    with open(f'random_sets/{i+1}/random_protein_set_{i+1}.fasta', 'w') as f:
       proteins = random.sample(human_proteome, 35)
       SeqIO.write(proteins, f, 'fasta')
 
 def search_random_sets():
   """The spike and shuffled peptides in the random sets."""
   for i in range(1000):
-    Preprocessor(f'random_sets/random_protein_set_{i+1}.fasta', 'pickle').preprocess(k=2)
-    Matcher('spike_9mers.fasta', f'random_protein_set_{i+1}', 5, 2).match()
-    Matcher('shuffled_9mers.fasta', f'random_protein_set_{i+1}', 5, 2).match()
-    Matcher('spike_15mers.fasta', f'random_protein_set_{i+1}', 7, 2).match()
-    Matcher('shuffled_15mers.fasta', f'random_protein_set_{i+1}', 7, 2).match()
+    random_set_path = f'random_sets/{i+1}'
+
+    # preprocess each random set
+    Preprocessor(f'{random_set_path}/random_protein_set_{i+1}.fasta', 'pickle', preprocessed_files_path=random_set_path).preprocess(k=2)
+
+    # search each random set for all peptides (spike/shuffled 9/15mers)
+    Matcher('spike_9mers.fasta', f'random_protein_set_{i+1}', 5, 2, prepreprocessed_files_path=random_set_path).match()
+    Matcher('shuffled_9mers.fasta', f'random_protein_set_{i+1}', 5, 2, prepreprocessed_files_path=random_set_path).match()
+    Matcher('spike_15mers.fasta', f'random_protein_set_{i+1}', 7, 2, prepreprocessed_files_path=random_set_path).match()
+    Matcher('shuffled_15mers.fasta', f'random_protein_set_{i+1}', 7, 2, prepreprocessed_files_path=random_set_path).match()
 
 def main():
   make_random_sets()
