79da45c7665dec257101c55bd9da97c081696378
Daniel Marrama <danielmarrama@gmail.com>
2023-03-22 12:30:40 -0700

    make sure pipeline works

--- myocarditis_antigen_search.py
+++ myocarditis_antigen_search.py
@@ -2,9 +2,14 @@
 
 from pepmatch import Preprocessor, Matcher
 
+import os
+
 # search the spike and shuffled peptides in the myocarditis-associated antigens
 Preprocessor('data/myocarditis_antigens.fasta', 'pickle').preprocess(k=2)
 Matcher('data/spike_9mers.fasta', 'data/myocarditis_antigens.fasta', 5, 2).match()
 Matcher('data/shuffled_9mers.fasta', 'data/myocarditis_antigens.fasta', 5, 2).match()
 Matcher('data/spike_15mers.fasta', 'data/myocarditis_antigens.fasta', 7, 2).match()
 Matcher('data/shuffled_15mers.fasta', 'data/myocarditis_antigens.fasta', 7, 2).match()
+
+os.remove('myocarditis_antigens_2mers.pickle')
+os.remove('myocarditis_antigens_names.pickle')
--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Getting data..."
 python3 get_data.py
+echo "Shuffling peptides..."
 python3 shuffled_peptides.py
+echo "Searching in myocarditis antigens..."
 python3 myocarditis_antigen_search.py
+echo "Creating and searching in random protein sets..."
 python3 random_sets.py
